6dd9a4401943d15891ce8d82aaf07c86f285d900
ANTHONY <anthony-nek30@outlook.com>
2020-01-11 23:56:45 -0500

    Mi primer commit

---
+++ MVC/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models
+{
+    public partial class Producto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El campo nombre es obligatorio")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El campo precio es obligatorio")]
+        public string Precio { get; set; }
+        [Required(ErrorMessage = "El campo tipo es obligatorio")]
+        public string Tipo { get; set; }
+    }
+}
---
+++ MVC/Models/bdEmpresaContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MVC.Models
+{
+    public partial class bdEmpresaContext : DbContext
+    {
+        public bdEmpresaContext()
+        {
+        }
+
+        public bdEmpresaContext(DbContextOptions<bdEmpresaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Producto> Producto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("data source = apicore.database.windows.net; initial catalog = bdEmpresa; user id =prueba; password = Pa$$w0rd2021");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Precio)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Tipo)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
